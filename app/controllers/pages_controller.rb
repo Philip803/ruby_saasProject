6d17b1f36cfd412cd33f1cf2c83f680f7b6a2e7d
Philip803 <30559200+Philip803@users.noreply.github.com>
2018-01-01 22:19:01 -0500

    Adde conditional logic on sign up form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+  # Get request for / which is our home page
   def home
+    @basic_plan = Plan.find(1).id
+    @pro_plan = Plan.find(2).id
   end
 
   def about
